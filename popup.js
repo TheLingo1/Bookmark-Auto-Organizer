de61e5d61337ad0c9d2e9ae7c725933c81d361af
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-05-19 08:51:57 -0500

    Make it so you can see how many protected bookmarks you have

--- popup.js
+++ popup.js
@@ -4,10 +4,14 @@ function setIndex(){
     numIndex = document.getElementById("numberInput").value;
     chrome.storage.local.set({key: numIndex}, function(){
         console.log("number stored")});
-        
+
     }
 
-    
+
+window.onload = function() {
+  chrome.storage.local.get('key', function(results) {
+    document.getElementById('numberInput').value = results.key;
+  });
+};
 
 document.getElementById('do-setindex').onclick = setIndex;
-
